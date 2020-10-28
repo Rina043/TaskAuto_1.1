c5874dee073d89be88ff04ba42c768da7c5cce9a
Ekaterina Shipkova <katerinashipkova@gmail.com>
2020-10-28 20:20:18 +0400

    tests added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,40 @@
+package ru.netology.service;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void shouldCalculateRemainIfAmountLoverBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCalculateRemainIfAmountHigherBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1100;
+
+        int actual = service.remain(amount);
+        int expected = 900;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCalculateRemainIfAmountIsBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        Assert.assertEquals(actual, expected);
+    }
+}
